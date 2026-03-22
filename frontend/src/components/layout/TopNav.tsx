d97d9353306603a45f773f7ca9ee54ab83e22b30
Alexander Hepburn <aahepburn@proton.me>
2026-03-22 19:37:42 +0100

    Fix missing useEffect import in TopNav.tsx (caused blank window on startup)

--- frontend/src/components/layout/TopNav.tsx
+++ frontend/src/components/layout/TopNav.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSessions } from "../../contexts/SessionsContext";
 import { useChatContext } from "../../contexts/ChatContext";
